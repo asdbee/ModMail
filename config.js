7b2cfd73018167f0075de4607331b6695c0732a7
Bee <54086109+asdbee@users.noreply.github.com>
2020-10-25 22:20:17 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    token: '',
+    databaseToken: '',
+
+    mainGuild: '',
+    logChannel: '',
+    mailChannel: '',
+    modRole: '',
+
+    status: 'for dms!',
+    color: 0xfcfcfc,
+    prefix: '!',
+    msgPrefix: 'Staff'
+}
