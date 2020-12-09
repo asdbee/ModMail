16c5eea539bd54fcffbe45b04abca60a0d55082a
asdbee <54086109+asdbee@users.noreply.github.com>
2020-12-09 13:06:27 -0500

    fix

--- commands/block.js
+++ commands/block.js
@@ -6,7 +6,6 @@ module.exports = {
     usage: '{prefix}block [id]',
     shortHands: [''],
     execute(bot,msg,args,checkMail){
-        if (checkMail === null) return bot.createMessage(msg.channel.id,'`!` There is no ModMail affiliated with this channel.')
         if (args[1] === undefined){
             mail.configBan('fromChannel',msg.channel.id,true)
             bot.createMessage(msg.channel.id,'`✔` Blocked '+msg.channel.guild.members.get(checkMail.userID).username)
--- commands/unblock.js
+++ commands/unblock.js
@@ -6,7 +6,6 @@ module.exports = {
     usage: '{prefix}unblock [id]',
     shortHands: [''],
     execute(bot,msg,args,checkMail){
-        if (checkMail === null) return bot.createMessage(msg.channel.id,'`!` There is no ModMail affiliated with this channel.')
         if (args[1] === undefined){
             mail.configBan('fromChannel',msg.channel.id,false)
             bot.createMessage(msg.channel.id,'`✔` Unblocked'+msg.channel.guild.members.get(checkMail.userID).username)
