3f0839801cf453014aadc64e5159c59ba149a58e
asdbee <54086109+asdbee@users.noreply.github.com>
2020-12-09 17:28:20 -0500

    Add image support to reply

--- commands/reply.js
+++ commands/reply.js
@@ -8,10 +8,13 @@ module.exports = {
     execute(bot,msg,args,checkMail){
         if (checkMail === null) return bot.createMessage(msg.channel.id,'`!` There is no ModMail affiliated with this channel.')
         const fullU = msg.author.username+'#'+msg.author.discriminator
+        let att = ''
+        if (msg.attachments[0] !== undefined){att = msg.attachments[0].filename+'\n'+msg.attachments[0].url}
+        else if (msg.attachments[0] === undefined){att = ''}
         if (args[1] === undefined) return;
         const content = msg.content.slice(config.prefix.length+args[0].length+1)
         bot.getDMChannel(checkMail.userID).then((bot) => bot.createMessage(config.msgPrefix+' **'+fullU+'**: '+content)).then(
-            bot.createMessage(msg.channel.id,config.msgPrefix+' **'+fullU+'**: '+content),
+            bot.createMessage(msg.channel.id,config.msgPrefix+' **'+fullU+'**: '+content+'\n'+att),
             msg.delete()
         )
     }
