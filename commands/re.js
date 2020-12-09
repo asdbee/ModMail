64cd9a5b08de2c3dc0876408d242725e8df8e85b
asdbee <54086109+asdbee@users.noreply.github.com>
2020-12-09 18:55:42 -0500

    Condensing

--- commands/re.js
+++ commands/re.js
@@ -11,17 +11,18 @@ module.exports = {
         if (checkMail === null) return bot.createMessage(msg.channel.id,'`!` There is no ModMail affiliated with this channel.')
 
         const get = path.join(__dirname, '../replies/'+args[1]+'.json');
-        const c = fs.readFileSync(get)
+        const c = require(get);
+        console.log(c.annon)
         const fullU = msg.author.username+'#'+msg.author.discriminator
-        if (JSON.parse(c.toString()).annon === false){
-        bot.getDMChannel(checkMail.userID).then((bot) => bot.createMessage(config.msgPrefix+' **'+fullU+'**: '+JSON.parse(c.toString()).reply)).then(
-            bot.createMessage(msg.channel.id,config.msgPrefix+' **'+fullU+'**: '+JSON.parse(c.toString()).reply),
+        if (c.annon === false){
+        bot.getDMChannel(checkMail.userID).then((bot) => bot.createMessage(config.msgPrefix+' **'+fullU+'**: '+c.reply)).then(
+            bot.createMessage(msg.channel.id,config.msgPrefix+' **'+fullU+'**: '+c.reply),
             msg.delete()
         )}
         
-        if (JSON.parse(c.toString()).annon === true){
-            bot.getDMChannel(checkMail.userID).then((bot) => bot.createMessage('**'+config.msgPrefix+'**: '+JSON.parse(c.toString()).reply)).then(
-                bot.createMessage(msg.channel.id,'(Annon) **'+fullU+'**: '+JSON.parse(c.toString()).reply),
+        if (c.annon === true){
+            bot.getDMChannel(checkMail.userID).then((bot) => bot.createMessage('**'+config.msgPrefix+'**: '+c.reply)).then(
+                bot.createMessage(msg.channel.id,'(Annon) **'+fullU+'**: '+c.reply),
                 msg.delete()
         )}
     }
