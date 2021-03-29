a5fa50466cbf8eb78ab5831c0a0658ade6968466
asdbee <54086109+asdbee@users.noreply.github.com>
2021-03-29 18:19:16 -0400

    !reply will now use nicknames if applicable

--- commands/reply.js
+++ commands/reply.js
@@ -1,4 +1,3 @@
-const mail = require('../modmail.js').get;
 const config = require('../config.js');
 module.exports = {
   name: 'reply',
@@ -7,7 +6,12 @@ module.exports = {
   shortHands: ['r'],
   execute(bot, msg, args, checkMail) {
     if (checkMail === null) return bot.createMessage(msg.channel.id, '`!` There is no ModMail affiliated with this channel.');
-    const fullU = msg.author.username + '#' + msg.author.discriminator;
+    let displayName = '';
+    if (msg.member.nick !== null) {
+      displayName = msg.member.nick;
+    } else if (msg.member.nick === null) {
+      displayName = msg.member.username + '#' + msg.member.discriminator;
+    }
     let att = '';
     if (msg.attachments[0] !== undefined) {
       att = msg.attachments[0].url;
@@ -18,7 +22,7 @@ module.exports = {
     const content = msg.content.slice(config.prefix.length + args[0].length + 1);
     bot
       .getDMChannel(checkMail.userID)
-      .then((bot) => bot.createMessage(config.msgPrefix + ' **' + fullU + '**: ' + content))
-      .then(bot.createMessage(msg.channel.id, config.msgPrefix + ' **' + fullU + '**: ' + content + '\n' + att), msg.delete());
+      .then((bot) => bot.createMessage(config.msgPrefix + ' **' + displayName + '**: ' + content))
+      .then(bot.createMessage(msg.channel.id, config.msgPrefix + ' **' + displayName + '**: ' + content + '\n' + att), msg.delete());
   },
 };
